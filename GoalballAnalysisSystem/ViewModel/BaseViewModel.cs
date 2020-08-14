f83954fe0ec648f0f08dbea7005423554ab7dae2
Zygimantas <zygimantas1997@gmail.com>
2020-08-14 09:22:31 +0300

    Added solution files

---
+++ GoalballAnalysisSystem/ViewModel/BaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace GoalballAnalysisSystem.ViewModel
+{
+    class BaseViewModel
+    {
+        
+        public ICommand UpdateSelectedViewModelCommand { get; private set; }
+
+        public BaseViewModel()
+        {
+            UpdateSelectedViewModelCommand = App.NavigationCommand;
+        }
+        
+    }
+}

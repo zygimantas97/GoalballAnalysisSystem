96e33d96d60487bdd923f8c1ab53fd3a2c14ee43
Zygimantas <zygimantas1997@gmail.com>
2020-08-17 17:10:11 +0300

    implemented basic get database methods

--- GoalballAnalysisSystem/ViewModel/DatabaseServices/SQLiteDatabaseService.cs
+++ GoalballAnalysisSystem/ViewModel/DatabaseServices/SQLiteDatabaseService.cs
@@ -1,4 +1,5 @@
-﻿using SQLite;
+﻿using GoalballAnalysisSystem.Model;
+using SQLite;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -53,6 +54,61 @@ namespace GoalballAnalysisSystem.ViewModel.DatabaseServices
             }
 
             return result;
+        }
+
+        public static User GetUser(string email)
+        {
+            User user = null;
+            using(SQLiteConnection connection = new SQLiteConnection(dbFile))
+            {
+                connection.CreateTable<User>();
+                user = connection.Table<User>().Where(u => u.Email == email).FirstOrDefault();
+            }
+            return user;
+        }
+
+        public static List<Player> GetPlayers(int userId)
+        {
+            List<Player> players = null;
+            using (SQLiteConnection connection = new SQLiteConnection(dbFile))
+            {
+                connection.CreateTable<Player>();
+                players = connection.Table<Player>().Where(p => p.UserId == userId).ToList();
+            }
+            return players;
+        }
+
+        public static List<Team> GetTeams(int userId)
+        {
+            List<Team> teams = null;
+            using (SQLiteConnection connection = new SQLiteConnection(dbFile))
+            {
+                connection.CreateTable<Team>();
+                teams = connection.Table<Team>().Where(t => t.UserId == userId).ToList();
+            }
+            return teams;
+        }
+
+        public static List<Game> GetGames(int userId)
+        {
+            List<Game> games = null;
+            using (SQLiteConnection connection = new SQLiteConnection(dbFile))
+            {
+                connection.CreateTable<Game>();
+                games = connection.Table<Game>().Where(g => g.UserId == userId).ToList();
+            }
+            return games;
+        }
+
+        public static List<Throw> GetThrows(int gameId)
+        {
+            List<Throw> throws = null;
+            using (SQLiteConnection connection = new SQLiteConnection(dbFile))
+            {
+                connection.CreateTable<Throw>();
+                throws = connection.Table<Throw>().Where(t => t.GameId == gameId).ToList();
+            }
+            return throws;
         }
     }
 }

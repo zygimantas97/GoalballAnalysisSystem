f7d6bc991eb47818ab6ceeb6da8a8c7e5b9b1bf7
zygimantas97 <zygimantas1997@gmail.com>
2021-03-03 21:58:11 +0200

    Added game player and projections data exchange services

--- ApiServiceTest/Program.cs
+++ ApiServiceTest/Program.cs
@@ -16,33 +16,87 @@ namespace ApiServiceTest
         static async Task MainAsync()
         {
             var identityService = new IdentityService();
+            var teamsService = new TeamsService(identityService);
+            var playersService = new PlayersService(identityService);
+            var teamPlayersService = new TeamPlayersService(identityService);
             var gamesService = new GamesService(identityService);
+            var gamePlayersService = new GamePlayersService(identityService);
+            var projectionsService = new ProjectionsService(identityService);
             try
             {
                 await identityService.LoginAsync("user@gas.com", "Password123!");
 
-                var createResponse = await gamesService.CreateGameAsync(new GameRequest
+                var teamResponse = await teamsService.CreateTeamAsync(new TeamRequest
                 {
-                    Title = "Test Game",
-                    Comment = "Test Game",
-                    HomeTeamId = null,
+                    Name = "Test team",
+                    Country = "LTU",
+                    Description = "Test team"
+                });
+                var playerResponse = await playersService.CreatePlayerAsync(new PlayerRequest
+                {
+                    Name = "Simas",
+                    Surname = "Simauskas",
+                    Country = "LTU",
+                    Description = "Test"
+                });
+                var teamPlayerResponse = await teamPlayersService.CreateTeamPlayerAsync(
+                    teamResponse.Id,
+                    playerResponse.Id,
+                    new TeamPlayerRequest
+                    {
+                        RoleId = 1,
+                        Number = 1
+                    });
+                var gameResponse = await gamesService.CreateGameAsync(new GameRequest
+                {
+                    Title = "Test",
+                    Comment = "Test",
+                    HomeTeamId = teamResponse.Id,
                     GuestTeamId = null
                 });
-                var getResponse = await gamesService.GetGameAsync(createResponse.Id);
-                Console.WriteLine(getResponse.Title);
-
-                var getAllResponse = await gamesService.GetGamesAsync();
-                Console.WriteLine(getAllResponse.Count);
-
-                await gamesService.UpdateGameAsync(getResponse.Id, new GameRequest
+                var gamePlayerResponse = await gamePlayersService.CreateGamePlayerAsync(new CreateGamePlayerRequest
                 {
-                    Title = "Test Game updated",
-                    Comment = "Test Game",
-                    HomeTeamId = null,
-                    GuestTeamId = null
+                    StartTime = DateTime.Now.AddSeconds(-10),
+                    EndTime = DateTime.Now,
+                    GameId = gameResponse.Id,
+                    TeamId = teamResponse.Id,
+                    PlayerId = playerResponse.Id
                 });
-                var deleteResponse = await gamesService.DeleteGameAsync(getResponse.Id);
-                Console.WriteLine(deleteResponse.Title);
+
+                // ----------
+
+                var createResponse = await projectionsService.CreateProjectionAsync(new ProjectionRequest
+                {
+                    X1 = 1,
+                    Y1 = 1,
+                    X2 = 100,
+                    Y2 = 100,
+                    Speed = 0,
+                    GameId = gameResponse.Id,
+                    DefenseGamePlayerId = null,
+                    OffenseGamePlayerId = gamePlayerResponse.Id
+                });
+                var getResponse = await projectionsService.GetProjectionAsync(createResponse.Id);
+                Console.WriteLine("X1: " + getResponse.X1);
+
+                var getByGameResponse = await projectionsService.GetProjectionsByGameAsync(gameResponse.Id);
+                Console.WriteLine(getByGameResponse.Count);
+                var getByGamePlayerResponse = await projectionsService.GetProjectionsByGamePlayerAsync(gamePlayerResponse.Id);
+                Console.WriteLine(getByGamePlayerResponse.Count);
+
+                await projectionsService.UpdateProjectionAsync(getResponse.Id, new ProjectionRequest
+                {
+                    X1 = 50,
+                    Y1 = 50,
+                    X2 = 100,
+                    Y2 = 100,
+                    Speed = 0,
+                    GameId = gameResponse.Id,
+                    DefenseGamePlayerId = null,
+                    OffenseGamePlayerId = gamePlayerResponse.Id
+                });
+                var deleteResponse = await projectionsService.DeleteProjectionAsync(getResponse.Id);
+                Console.WriteLine("X1: " + deleteResponse.X1);
 
                 Console.WriteLine("Ok");
             }
--- GoalballAnalysisSystem.WPF/Services/ProjectionsService.cs
+++ GoalballAnalysisSystem.WPF/Services/ProjectionsService.cs
@@ -1,7 +1,11 @@
 ﻿using GoalballAnalysisSystem.API.Contracts.V1.Requests;
 using GoalballAnalysisSystem.API.Contracts.V1.Responses;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,40 +14,132 @@ namespace GoalballAnalysisSystem.WPF.Services
     public class ProjectionsService : BaseService
     {
         private readonly IIdentityService _identityService;
+        private readonly string _serviceUrl;
 
         public ProjectionsService(IIdentityService identityService)
         {
             _identityService = identityService;
+            _serviceUrl = ApiUrl + "Projections/";
         }
 
         public async Task<List<ProjectionResponse>> GetProjectionsByGameAsync(long gameId)
         {
-            return null;
+            Uri uri = new Uri(_serviceUrl + "ByGame/" + gameId);
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", await _identityService.GetToken());
+                var response = await client.GetAsync(uri);
+                var responseString = await response.Content.ReadAsStringAsync();
+                if (response.IsSuccessStatusCode)
+                {
+                    return JsonConvert.DeserializeObject<List<ProjectionResponse>>(responseString);
+                }
+                else
+                {
+                    var errorResponse = JsonConvert.DeserializeObject<ErrorResponse>(responseString);
+                    throw new Exception(string.Join('\n', errorResponse.Errors.Select(e => e.Message)));
+                }
+            }
         }
 
         public async Task<List<ProjectionResponse>> GetProjectionsByGamePlayerAsync(long gamePlayerId)
         {
-            return null;
+            Uri uri = new Uri(_serviceUrl + "ByGamePlayer/" + gamePlayerId);
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", await _identityService.GetToken());
+                var response = await client.GetAsync(uri);
+                var responseString = await response.Content.ReadAsStringAsync();
+                if (response.IsSuccessStatusCode)
+                {
+                    return JsonConvert.DeserializeObject<List<ProjectionResponse>>(responseString);
+                }
+                else
+                {
+                    var errorResponse = JsonConvert.DeserializeObject<ErrorResponse>(responseString);
+                    throw new Exception(string.Join('\n', errorResponse.Errors.Select(e => e.Message)));
+                }
+            }
         }
 
         public async Task<ProjectionResponse> GetProjectionAsync(long projectionId)
         {
-            return null;
+            Uri uri = new Uri(_serviceUrl + projectionId);
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", await _identityService.GetToken());
+                var response = await client.GetAsync(uri);
+                var responseString = await response.Content.ReadAsStringAsync();
+                if (response.IsSuccessStatusCode)
+                {
+                    return JsonConvert.DeserializeObject<ProjectionResponse>(responseString);
+                }
+                else
+                {
+                    var errorResponse = JsonConvert.DeserializeObject<ErrorResponse>(responseString);
+                    throw new Exception(string.Join('\n', errorResponse.Errors.Select(e => e.Message)));
+                }
+            }
         }
 
         public async Task UpdateProjectionAsync(long projectionId, ProjectionRequest request)
         {
-
+            var jsonRequest = JsonConvert.SerializeObject(request);
+            HttpContent content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+            Uri uri = new Uri(_serviceUrl + projectionId);
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", await _identityService.GetToken());
+                var response = await client.PutAsync(uri, content);
+                var responseString = await response.Content.ReadAsStringAsync();
+                if (!response.IsSuccessStatusCode)
+                {
+                    var errorResponse = JsonConvert.DeserializeObject<ErrorResponse>(responseString);
+                    throw new Exception(string.Join('\n', errorResponse.Errors.Select(e => e.Message)));
+                }
+            }
         }
 
         public async Task<ProjectionResponse> CreateProjectionAsync(ProjectionRequest request)
         {
-            return null;
+            var jsonRequest = JsonConvert.SerializeObject(request);
+            HttpContent content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+            Uri uri = new Uri(_serviceUrl);
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", await _identityService.GetToken());
+                var response = await client.PostAsync(uri, content);
+                var responseString = await response.Content.ReadAsStringAsync();
+                if (response.IsSuccessStatusCode)
+                {
+                    return JsonConvert.DeserializeObject<ProjectionResponse>(responseString);
+                }
+                else
+                {
+                    var errorResponse = JsonConvert.DeserializeObject<ErrorResponse>(responseString);
+                    throw new Exception(string.Join('\n', errorResponse.Errors.Select(e => e.Message)));
+                }
+            }
         }
 
         public async Task<ProjectionResponse> DeleteProjectionAsync(long projectionId)
         {
-            return null;
+            Uri uri = new Uri(_serviceUrl + projectionId);
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", await _identityService.GetToken());
+                var response = await client.DeleteAsync(uri);
+                var responseString = await response.Content.ReadAsStringAsync();
+                if (response.IsSuccessStatusCode)
+                {
+                    return JsonConvert.DeserializeObject<ProjectionResponse>(responseString);
+                }
+                else
+                {
+                    var errorResponse = JsonConvert.DeserializeObject<ErrorResponse>(responseString);
+                    throw new Exception(string.Join('\n', errorResponse.Errors.Select(e => e.Message)));
+                }
+            }
         }
     }
 }

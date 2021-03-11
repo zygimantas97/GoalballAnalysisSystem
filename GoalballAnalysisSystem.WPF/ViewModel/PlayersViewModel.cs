2e7a57c709894324fa9831a4b8030e8a32f77eb0
JuliusKTU <gudauskas.julius@gmail.com>
2021-03-11 21:48:33 +0200

    Added player GUI

--- GoalballAnalysisSystem.WPF/ViewModel/PlayersViewModel.cs
+++ GoalballAnalysisSystem.WPF/ViewModel/PlayersViewModel.cs
@@ -1,11 +1,88 @@
-﻿using GoalballAnalysisSystem.WPF.ViewModel;
+﻿using GoalballAnalysisSystem.Domain.Models;
+using GoalballAnalysisSystem.WPF.Commands;
+using GoalballAnalysisSystem.WPF.Services;
+using GoalballAnalysisSystem.WPF.State.Authenticators;
+using GoalballAnalysisSystem.WPF.State.Navigators;
+using GoalballAnalysisSystem.WPF.ViewModel;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace GoalballAnalysisSystem.WPF.ViewModel
 {
     public class PlayersViewModel : BaseViewModel
     {
+
+        public ICommand LoginCommand { get; }
+        public ICommand UpdateCurrentViewModelCommand { get; }
+        public ICommand ChangeSelectedPlayer { get; }
+        public ICommand EditSelectedPlayer { get; }
+        public ICommand DeleteSelectedPlayer { get; }
+        public ICommand AddNewPlayerPlayer { get; }
+
+        private readonly ObservableCollection<Player> listOfItems;
+
+        private Player _selectedPlayer;
+        public Player SelectedPlayer
+        {
+            get
+            {
+                return _selectedPlayer;
+            }
+            private set
+            {
+                _selectedPlayer = value;
+                OnPropertyChanged(nameof(SelectedPlayer));
+            }
+        }
+
+        public ObservableCollection<Player> ListOfItems
+        {
+            get { return listOfItems; }
+        }
+
+
+        public PlayersViewModel()
+        {
+            listOfItems = AddFakeData();
+            if (listOfItems.Count != 0)
+                SelectedPlayer = listOfItems[0];
+
+
+
+            ChangeSelectedPlayer = new ChangeSelectedPlayer(this);
+        }
+
+        public ObservableCollection<Player> AddFakeData()
+        {
+            ObservableCollection<Player> list = new ObservableCollection<Player>();
+            
+
+            for(int i= 0; i< 50; i++)
+            {
+                Player a = new Player();
+                a.Name = "Vardas " + i;
+                a.Surname = "Pavarde " + i;
+                a.Id = i;
+                list.Add(a);
+            }
+
+            return list;
+        }
+
+        public void SelectPlayer(int Id)
+        {
+            foreach (Player item in listOfItems)
+            {
+                if(item.Id == Id)
+                {
+                    SelectedPlayer = item;
+                }
+            }
+        }
+
     }
 }

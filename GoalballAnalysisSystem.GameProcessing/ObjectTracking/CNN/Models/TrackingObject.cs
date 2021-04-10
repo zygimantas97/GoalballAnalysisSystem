f357460d53260ca240ef009d7248776a91172b21
zygimantas97 <zygimantas1997@gmail.com>
2021-04-10 22:26:08 +0300

    Naming convention related refactoring in CNNBasedMOT

--- GoalballAnalysisSystem.GameProcessing/ObjectTracking/CNN/Models/TrackingObject.cs
+++ GoalballAnalysisSystem.GameProcessing/ObjectTracking/CNN/Models/TrackingObject.cs
@@ -10,9 +10,9 @@ namespace GoalballAnalysisSystem.GameProcessing.ObjectTracking.CNN.Models
         public T Object { get; private set; }
         public Rectangle BoundingBox { get; private set; }
 
-        private readonly List<double> xValues = new List<double>();
-        private readonly List<double> yValues = new List<double>();
-        private readonly List<Rectangle> predictedBoundaries = new List<Rectangle>();
+        private readonly List<double> _xValues = new List<double>();
+        private readonly List<double> _yValues = new List<double>();
+        private readonly List<Rectangle> _predictedBoundaries = new List<Rectangle>();
 
         private readonly int _pointCountInRegression;
 
@@ -22,55 +22,42 @@ namespace GoalballAnalysisSystem.GameProcessing.ObjectTracking.CNN.Models
             BoundingBox = roi;
             _pointCountInRegression = pointCountInRegression;
 
-            xValues.Add(0);
-            yValues.Add(0);
+            _xValues.Add(0);
+            _yValues.Add(0);
 
-            xValues.Add(roi.X + (roi.Width / 2));
-            yValues.Add(roi.Y + (roi.Height / 2));
-        }
-
-        public void Update(Rectangle newROI)
-        {
-            BoundingBox = newROI;
-            xValues.Add(newROI.X + (newROI.Width / 2));
-            yValues.Add(newROI.Y + (newROI.Height / 2));
-
-            if (xValues.Count >= _pointCountInRegression)
-            {
-                xValues.RemoveAt(0);
-                yValues.RemoveAt(0);
-            }
+            _xValues.Add(roi.X + (roi.Width / 2));
+            _yValues.Add(roi.Y + (roi.Height / 2));
         }
 
         public double DistanceToPreviousPoint(Rectangle rect, int index)
         {
             double distance = 0;
-            if (xValues.Count - index >= 0)
-                distance = Math.Pow(Math.Pow(xValues[xValues.Count - index] - (rect.X + (rect.Width / 2)), 2) + Math.Pow(yValues[yValues.Count - index] - (rect.Y + (rect.Height / 2)), 2), 0.5);
+            if (_xValues.Count - index >= 0)
+                distance = Math.Pow(Math.Pow(_xValues[_xValues.Count - index] - (rect.X + (rect.Width / 2)), 2) + Math.Pow(_yValues[_yValues.Count - index] - (rect.Y + (rect.Height / 2)), 2), 0.5);
 
             return distance;
         }
 
         public void AddPredictionBoundary(Rectangle boundary)
         {
-            predictedBoundaries.Add(boundary);
+            _predictedBoundaries.Add(boundary);
         }
 
         public void ClearPredictionBoundaries()
         {
-            predictedBoundaries.Clear();
+            _predictedBoundaries.Clear();
         }
 
         public void DetermineAndUpdateMostFittingPrediction()
         {
-            if (predictedBoundaries.Count > 0)
+            if (_predictedBoundaries.Count > 0)
             {
-                var distance = Math.Pow(Math.Pow(xValues[xValues.Count - 1] - (predictedBoundaries[0].X + (predictedBoundaries[0].Width / 2)), 2) + Math.Pow(yValues[yValues.Count - 1] - (predictedBoundaries[0].Y + (predictedBoundaries[0].Height / 2)), 2), 0.5);
-                var currentBest = predictedBoundaries[0];
+                var distance = Math.Pow(Math.Pow(_xValues[_xValues.Count - 1] - (_predictedBoundaries[0].X + (_predictedBoundaries[0].Width / 2)), 2) + Math.Pow(_yValues[_yValues.Count - 1] - (_predictedBoundaries[0].Y + (_predictedBoundaries[0].Height / 2)), 2), 0.5);
+                var currentBest = _predictedBoundaries[0];
 
-                foreach (var boundary in predictedBoundaries) //select closest to the previous ROI
+                foreach (var boundary in _predictedBoundaries) //select closest to the previous ROI
                 {
-                    var dist = Math.Pow(Math.Pow(xValues[xValues.Count - 1] - (boundary.X + (boundary.Width / 2)), 2) + Math.Pow(yValues[yValues.Count - 1] - (boundary.Y + (boundary.Height / 2)), 2), 0.5);
+                    var dist = Math.Pow(Math.Pow(_xValues[_xValues.Count - 1] - (boundary.X + (boundary.Width / 2)), 2) + Math.Pow(_yValues[_yValues.Count - 1] - (boundary.Y + (boundary.Height / 2)), 2), 0.5);
                     if (dist < distance)
                     {
                         distance = dist;
@@ -78,6 +65,19 @@ namespace GoalballAnalysisSystem.GameProcessing.ObjectTracking.CNN.Models
                     }
                 }
                 Update(currentBest);
+            }
+        }
+
+        private void Update(Rectangle newROI)
+        {
+            BoundingBox = newROI;
+            _xValues.Add(newROI.X + (newROI.Width / 2));
+            _yValues.Add(newROI.Y + (newROI.Height / 2));
+
+            if (_xValues.Count >= _pointCountInRegression)
+            {
+                _xValues.RemoveAt(0);
+                _yValues.RemoveAt(0);
             }
         }
     }
